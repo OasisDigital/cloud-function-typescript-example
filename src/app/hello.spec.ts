46765cb6dcaa6c8ecbcc2b77a7fe2c3ba50af5bf
Kyle Cordes <kyle.cordes@oasisdigital.com>
2017-03-24 21:35:39 -0700

    Use moment calculation in still-trivial hello world

--- src/app/hello.spec.ts
+++ src/app/hello.spec.ts
@@ -4,6 +4,6 @@ import { helloFn } from './hello';
 
 describe('Hello function', () => {
   it('returns hello world', () => {
-    expect(helloFn()).to.eql('hello, world');
+    expect(helloFn()).to.eql('hello, world, the answer is 42');
   });
 });
--- src/app/hello.ts
+++ src/app/hello.ts
@@ -1,6 +1,11 @@
 // Here's the core function of our hello world example. There is no use of any
 // cloud function API here.
 
+import * as moment from 'moment';
+
 export function helloFn() {
-  return 'hello, world';
+  const now = moment();
+  const later = moment().add(6, 'w');
+  const n = later.diff(now, 'd');
+  return 'hello, world, the answer is ' + n;
 }
